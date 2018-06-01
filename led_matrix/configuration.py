0209d7305842a570cb56ec4e4a809d326ddf3d14
Barry McAndrews <bmcandrews3@hotmail.com>
2018-06-01 13:58:01 -0400

    add home to configuration

--- led_matrix/configuration.py
+++ led_matrix/configuration.py
@@ -7,6 +7,7 @@ parser.read('defaults.conf')
 # Section Titles
 MATRIX = 'matrix'
 MQTT = 'mqtt'
+HOME = 'home'
 
 # Matrix Settings
 matrix_options = rgbmatrix.RGBMatrixOptions()
@@ -16,6 +17,7 @@ matrix_options.cols = parser[MATRIX].getint('cols')
 matrix_options.chain_length = parser[MATRIX].getint('chain_length')
 matrix_options.parallel = parser[MATRIX].getint('parallel')
 matrix_options.hardware_mapping = parser[MATRIX].get('hardware_mapping')
+matrix_options.scan_mode = 1
 
 
 # MQTT Settings
@@ -24,6 +26,15 @@ class MqttOptions(object):
     ca_file: str = parser[MQTT].get('ca_file')
     client_cert_file: str = parser[MQTT].get('client_cert_file')
     client_key_file: str = parser[MQTT].get('client_key_file')
+    topic_prefix: str = parser[MQTT].get('topic_prefix')
 
 
-mqtt_options = MqttOptions()
+# Home Settings
+class HomeOptions(object):
+    enabled: bool = parser[HOME].getboolean('enabled', fallback=False)
+    show_time: bool = parser[HOME].getboolean('show_time', fallback=False)
+    show_weather: bool = parser[HOME].getboolean('show_weather', fallback=False)
+    show_news: bool = parser[HOME].getboolean('show_news', fallback=False)
+    show_spotify: bool = parser[HOME].getboolean('show_spotify', fallback=False)
+    show_stocks: bool = parser[HOME].getboolean('show_spotify', fallback=False)
+
